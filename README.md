56902369a132cc12d869cbe0b6d3b76432b6eabe
zhangdongya <fortitude.zhang@gmail.com>
2022-05-30 23:36:44 +0800

    [c4n] init the c4n.reroute.cc web site.

---
+++ README.md
@@ -0,0 +1,5 @@
+# convertor4net
+
+Source code for convertor4net website, an online misc convertor for coder focused in networking.
+
+[Convertor4net](https://c4n.reroute.cc)
---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import {ref, computed } from "vue"
+
+let hexInput = ref("")
+let binaryWithPos = ref("")
+
+const convertHex2BinaryWithPos = () => {
+    // from hex to a binary representation.
+    let result = parseInt(hexInput.value, 16).toString(2)
+    let length = result.length
+    let convertResult = ""
+    // to a format bPos:1,bPos,0 style string.
+    for (let pos = 0; pos < length; pos++) {
+        convertResult = convertResult + "@" + (length - 1 - pos) +":"+result[pos] + ","
+    }
+    binaryWithPos.value = "[" + convertResult + "]"
+}
+
+</script>
+
+<template>
+  <header class="container mx-auto pt-8">
+    <h1 class="font-mono text-xl"> Online convertor for programmer focused on networking. </h1>
+  </header>
+<div class="container mx-auto pt-20">
+  <p class="font-mono text-lg">Hex2binary with bit position is: </p>
+  <p class="font-mono text-lg text-red-500 break-all">{{ binaryWithPos }}</p>
+  <input class="rounded-lg font-mono test-lg" v-model="hexInput" placeholder="Input hex number" @keyup="convertHex2BinaryWithPos" />
+</div>
+</template>
+
+<style>
+@import '@/assets/base.css';
+</style>
